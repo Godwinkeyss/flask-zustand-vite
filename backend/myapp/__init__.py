e876c53e66db243d1f1b4f16035d3ad32ddb438d
godwinkeyss <godwinkeyss@gmail.com>
2025-02-12 00:14:47 +0000

    flask migrate

--- backend/myapp/__init__.py
+++ backend/myapp/__init__.py
@@ -2,13 +2,13 @@ from flask import Flask, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
-
+from flask_migrate import Migrate
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
 db=SQLAlchemy(app)
-
+migrate = Migrate(app,db)
 
 # Correct Static File Path
 backend_folder = os.path.dirname(os.path.abspath(__file__))  # myapp/
